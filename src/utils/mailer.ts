f794019274d1e4e3633906c1b9af9b50c975810b
thanhtike-aung <th.aung25@hotmail.com>
2025-06-01 21:15:09 +0630

    email test

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -16,6 +16,9 @@ interface EmailOptions {
 
 export const sendEmail = async (options: EmailOptions): Promise<void> => {
   try {
+    console.log(process.env.EMAIL_USER);
+    console.log(process.env.EMAIL_PASSWORD);
+    console.log(options);
     await transporter.sendMail({
       from: `"Dev02 Report System" <${process.env.EMAIL_USER}>`,
       to: options.to,
